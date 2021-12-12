44675bdc78ad8c7a508447ae51eeba1043ebc235
kyu9341 <kyu9341@naver.com>
2021-12-12 16:36:16 +0900

    feat(CB-1): dashboard page 생성

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/common/layout';
+import Typography from '@mui/material/Typography';
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <Typography variant="h3">Dashboard</Typography>
+    </Layout>
+  );
+}
