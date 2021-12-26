6c36f7ac64a8c23fe0d0d4cf8bc4ae9928945b2a
kyu9341 <kyu9341@naver.com>
2021-12-26 18:54:37 +0900

    refactor(CB-10): bot summary 분리

--- src/components/my-bot-summary/bot-list.tsx
+++ src/components/my-bot-summary/bot-list.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useMyBotList } from '@/hooks/bot';
-import { IBotSummary } from '@/schema/bot';
-import Switch from '@mui/material/Switch';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -10,35 +8,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { beforeNow } from '@/utils/date';
-import RunningTime from './running-time';
+import BotSummary from './bot-summary';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
   },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
 }));
 
-const EarningRateSpan = styled('span', {
-  shouldForwardProp: prop => prop !== 'positive',
-})<{ positive?: boolean }>(({ positive }) => ({
-  color: positive ? 'red' : 'blue',
-  fontWeight: 500,
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
 const TITLES = ['봇', '마켓', '상태', '실행 시간', '알고리즘', '수익률'];
 
 function BotList() {
@@ -65,50 +43,6 @@ function BotList() {
         </TableBody>
       </Table>
     </TableContainer>
-  );
-}
-
-interface IBotSummaryProps {
-  bot: IBotSummary;
-}
-
-const ON_OFF_STATUS = {
-  running: true,
-  preparing: true,
-  trading: true,
-  paused: false,
-  error: false,
-  terminating: false,
-};
-
-function BotSummary({ bot }: IBotSummaryProps) {
-  const { name, algorithm, earningRate, market, startTime, status } = bot;
-  const positive = earningRate > 0;
-  const [checked, setChecked] = useState<boolean>(ON_OFF_STATUS[status]);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(e.target.checked);
-  };
-
-  return (
-    <StyledTableRow>
-      <StyledTableCell size="small" sx={{ width: '80px', pr: 0 }}>
-        <Switch color="warning" checked={checked} onChange={handleChange} />
-      </StyledTableCell>
-      <StyledTableCell sx={{ pl: 0 }} align="center">
-        {name}
-      </StyledTableCell>
-      <StyledTableCell align="center">{market}</StyledTableCell>
-      <StyledTableCell align="center">{status}</StyledTableCell>
-      <StyledTableCell align="center">
-        <RunningTime startTime={startTime} />
-      </StyledTableCell>
-      <StyledTableCell align="center">{algorithm}</StyledTableCell>
-      <StyledTableCell align="center">
-        <EarningRateSpan positive={positive}>{`${
-          positive ? '+' : ''
-        }${earningRate}%`}</EarningRateSpan>
-      </StyledTableCell>
-    </StyledTableRow>
   );
 }
 
---
+++ src/components/my-bot-summary/bot-summary.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { IBotSummary } from '@/schema/bot';
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import RunningTime from './running-time';
+
+const StyledTableCell = styled(TableCell)(() => ({
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const EarningRateSpan = styled('span', {
+  shouldForwardProp: prop => prop !== 'positive',
+})<{ positive?: boolean }>(({ positive }) => ({
+  color: positive ? 'red' : 'blue',
+  fontWeight: 500,
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+const ON_OFF_STATUS = {
+  running: true,
+  preparing: true,
+  trading: true,
+  paused: false,
+  error: false,
+  terminating: false,
+};
+
+interface IBotSummaryProps {
+  bot: IBotSummary;
+}
+
+function BotSummary({ bot }: IBotSummaryProps) {
+  const { name, algorithm, earningRate, market, startTime, status } = bot;
+  const positive = earningRate > 0;
+  const [checked, setChecked] = useState<boolean>(ON_OFF_STATUS[status]);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+  };
+
+  return (
+    <StyledTableRow>
+      <StyledTableCell size="small" sx={{ width: '80px', pr: 0 }}>
+        <Switch color="warning" checked={checked} onChange={handleChange} />
+      </StyledTableCell>
+      <StyledTableCell sx={{ pl: 0 }} align="center">
+        {name}
+      </StyledTableCell>
+      <StyledTableCell align="center">{market}</StyledTableCell>
+      <StyledTableCell align="center">{status}</StyledTableCell>
+      <StyledTableCell align="center">
+        <RunningTime startTime={startTime} />
+      </StyledTableCell>
+      <StyledTableCell align="center">{algorithm}</StyledTableCell>
+      <StyledTableCell align="center">
+        <EarningRateSpan positive={positive}>{`${
+          positive ? '+' : ''
+        }${earningRate}%`}</EarningRateSpan>
+      </StyledTableCell>
+    </StyledTableRow>
+  );
+}
+
+export default BotSummary;
