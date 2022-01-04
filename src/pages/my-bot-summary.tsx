5cabacbba0903abbfa88424c28818dbbc3ed83d6
kyu9341 <kyu9341@naver.com>
2022-01-04 23:50:49 +0900

    feat(CB-36): 코인 봇 생성 버튼 추가

--- src/pages/my-bot-summary.tsx
+++ src/pages/my-bot-summary.tsx
@@ -1,9 +1,23 @@
 import Layout from '@/components/common/layout';
 import BotList from '@/components/my-bot-summary/bot-list';
+import { Box, Button, Typography } from '@mui/material';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 
 export default function MyBotSummary() {
   return (
     <Layout>
+      <Box sx={{ padding: 1, display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h4">My Bot List</Typography>
+        <Button
+          sx={{ m: 1 }}
+          size="small"
+          variant="contained"
+          color="warning"
+          startIcon={<AddRoundedIcon />}
+        >
+          new
+        </Button>
+      </Box>
       <BotList />
     </Layout>
   );
