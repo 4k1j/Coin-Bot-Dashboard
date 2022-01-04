a8523714ac346b340b6bca23862535b7b62d9fbf
kyu9341 <kyu9341@naver.com>
2022-01-05 01:07:22 +0900

    feat(CB-36): coin-bot 생성 페이지 링크 추가

--- src/pages/my-bot-summary.tsx
+++ src/pages/my-bot-summary.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Layout from '@/components/common/layout';
 import BotList from '@/components/my-bot-summary/bot-list';
 import { Box, Button, Typography } from '@mui/material';
@@ -8,15 +9,17 @@ export default function MyBotSummary() {
     <Layout>
       <Box sx={{ padding: 1, display: 'flex', justifyContent: 'space-between' }}>
         <Typography variant="h4">My Bot List</Typography>
-        <Button
-          sx={{ m: 1 }}
-          size="small"
-          variant="contained"
-          color="warning"
-          startIcon={<AddRoundedIcon />}
-        >
-          new
-        </Button>
+        <Link href="/coin-bot/new" passHref>
+          <Button
+            sx={{ m: 1 }}
+            size="small"
+            variant="contained"
+            color="warning"
+            startIcon={<AddRoundedIcon />}
+          >
+            new
+          </Button>
+        </Link>
       </Box>
       <BotList />
     </Layout>
