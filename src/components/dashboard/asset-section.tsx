f5c858e60195feaf2fc63c781220942bc803d2e2
kyu9341 <kyu9341@naver.com>
2022-01-30 22:43:34 +0900

    feat(CB-50): main 페이지 UI grid적용

---
+++ src/components/dashboard/asset-section.tsx
@@ -0,0 +1,11 @@
+import { Box, Paper } from '@mui/material';
+
+function AssetSection() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Paper variant="outlined" />
+    </Box>
+  );
+}
+
+export default AssetSection;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,44 @@
 import Layout from '@/components/common/layout';
-import Typography from '@mui/material/Typography';
+import { styled, useTheme } from '@mui/material/styles';
+import { Grid, Paper, Box } from '@mui/material';
+import AssetSection from '@/components/dashboard/asset-section';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: '100%',
+}));
 
 export default function Dashboard() {
+  const theme = useTheme();
+
   return (
     <Layout>
-      <Typography variant="h3">Dashboard</Typography>
+      <Box
+        sx={{
+          width: '100%',
+          height: `calc(100vh - ${theme.header.height}px - ${theme.spacing(2)})`,
+        }}
+      >
+        <Grid container rowSpacing={2} columnSpacing={2} sx={{ height: '100%' }}>
+          <Grid item xs={6}>
+            <Item>
+              <AssetSection />
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>2</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>3</Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>4</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </Layout>
   );
 }
